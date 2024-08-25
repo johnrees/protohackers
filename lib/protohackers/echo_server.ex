63d8380e949244cfa22eb3cc5e97d68c104a3db3
John Rees <john@bitsushi.com>
2024-08-25 13:23:22 +0100

    pre Task.supervisor

---
+++ lib/protohackers/echo_server.ex
@@ -0,0 +1,66 @@
+defmodule Protohackers.EchoServer do
+  use GenServer
+
+  require Logger
+
+  def start_link([] = _opts) do
+    GenServer.start_link(__MODULE__, :no_state)
+  end
+
+  defstruct [:listen_socket]
+
+  @impl true
+  def init(:no_state) do
+    listen_options = [
+      ifaddr: {0, 0, 0, 0},
+      mode: :binary,
+      active: false,
+      reuseaddr: true,
+      exit_on_close: false
+    ]
+
+    case :gen_tcp.listen(5005, listen_options) do
+      {:ok, listen_socket} ->
+        Logger.info("Server running on port 5005")
+        state = %__MODULE__{listen_socket: listen_socket}
+        {:ok, state, {:continue, :accept}}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  @impl true
+  def handle_continue(:accept, %__MODULE__{} = state) do
+    case :gen_tcp.accept(state.listen_socket) do
+      {:ok, socket} ->
+        handle_connection(socket)
+        {:noreply, state, {:continue, :accept}}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  # Helpers
+
+  defp handle_connection(socket) do
+    case recv_until_closed(socket, _buffer = "", _buffered_size = 0) do
+      {:ok, data} -> :gen_tcp.send(socket, data)
+      {:error, reason} -> Logger.error("Failed to receive data: #{inspect(reason)}")
+    end
+
+    :gen_tcp.close(socket)
+  end
+
+  @limit _100_kb = 1024 * 100
+
+  defp recv_until_closed(socket, buffer, buffered_size) do
+    case :gen_tcp.recv(socket, 0, 10_000) do
+      {:ok, data} when buffered_size + byte_size(data) > @limit -> {:error, :buffer_overflow}
+      {:ok, data} -> recv_until_closed(socket, [buffer, data], buffered_size + byte_size(data))
+      {:error, :closed} -> {:ok, buffer}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+end
---
+++ test/protohackers/echo_server_test.exs
@@ -0,0 +1,17 @@
+defmodule Protohackers.EchoServerTest do
+  use ExUnit.Case
+
+  test "echoes anything back" do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 5005, mode: :binary, active: false)
+    assert :gen_tcp.send(socket, "hello") == :ok
+    assert :gen_tcp.send(socket, "world") == :ok
+    :gen_tcp.shutdown(socket, :write)
+    assert :gen_tcp.recv(socket, 0, 5000) == {:ok, "helloworld"}
+  end
+
+  test "echo server has a max buffer size" do
+    {:ok, socket} = :gen_tcp.connect(~c"localhost", 5005, mode: :binary, active: false)
+    assert :gen_tcp.send(socket, :binary.copy("a", 1024 * 100 + 1)) == :ok
+    assert :gen_tcp.recv(socket, 0) == {:error, :closed}
+  end
+end
